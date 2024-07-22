27fc787518d584a2a987932d672e6960ee2b37ec
Tomas Galotti <galottit@gmail.com>
2024-07-22 12:51:53 -0300

    se creo js para que guarde los archivos en un txt cuando se aprete el boton de enviar

---
+++ scripts/contacto.js
@@ -0,0 +1,21 @@
+let nombre = document.getElementById("nombre");
+let apellido = document.getElementById("apellido");
+let email = document.getElementById("email");
+let telefono = document.getElementById("telefono");
+let consulta = document.getElementById("cajaConsulta");
+let botonEnviar = document.getElementById("botonEnviar");
+let formularioContacto = [];
+
+botonEnviar.addEventListener("click", () => {
+    formularioContacto[0] = nombre.value;
+    formularioContacto[1] = apellido.value;
+    formularioContacto[2] = email.value;
+    formularioContacto[3] = telefono.value;
+    formularioContacto[4] = consulta.value;
+
+    let blob = new Blob([formularioContacto], {type: "text/plain;charset=utf-8"}); // navegador
+
+    //Libreria FileSaver.js
+       saveAs(blob, "contact.txt");
+       console.log(blob)
+})
