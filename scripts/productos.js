df60dc274af18f727802f8e05c5af14e70594295
Aldo Lopez <aldo.lopez@gmail.com>
2024-07-22 14:25:48 +0000

    Cambio en carga de productos y ofertas (Templates diferenciados)

--- scripts/productos.js
+++ scripts/productos.js
@@ -3,52 +3,62 @@
 let arrProductosNombres = ["Fideos Codito", "Fideos Moño", "Fideos Tallarines", "Arroz Fino Largo", "Arroz Doble Carolina", "Shampoo", "Crema de Enjuague", "Desodorante", "Jabón de Tocador", "Dentrífico", "Queso Cremoso", "Yogourt Bebible", "Yogourt Natural", "Queso Rayado", "Leche Larga Vida", "Lavandina", "Desodorante de Pisos", "Desengrasante", "Detergente", "Perfume de Ambiente", "Plato", "Vaso", "Cuchillo", "Tenedor", "Rejilla", "Alimento para Gatos", "Alimento para Perros", "Piedras Sanitarias", "Bebedero", "Plato para Alimento", "Camisa Manga Larga", "Chomba", "Pantalón Cargo", "Remera Lisa", "Zapatillas", "Fernet", "Vino", "Vodka", "Ron", "Gin", "Osito de Peluche", "Camión Volcador", "Paleta de Ping Pong", "Pistola de Agua", "Dron a Batería", "Pan", "Bizcochos", "Galletitas Secas", "Galletitas Integrales", "Tostadas"];
 let arrProductosPrecios = [20.00, 25.00, 22.00, 28.00, 30.00, 31.90, 34.20, 36.50, 38.80, 41.10, 43.40, 45.70, 48.00, 50.30, 52.60, 54.90, 57.20, 30.00, 31.90, 34.20, 36.50, 38.80, 40.94, 43.16, 45.38, 47.60, 49.82, 52.04, 54.26, 56.48, 58.70, 60.92, 63.14, 54.90, 57.20, 30.00, 31.90, 34.20, 36.50, 38.80, 41.10, 43.40, 45.70, 48.00, 50.30, 52.60, 54.90, 57.20, 59.50, 61.80];
 let arrProductosStock = [8, 10, 3, 5, 9, 2, 4, 5, 0, 6, 7, 1, 10, 5, 2, 7, 6, 0, 9, 1, 8, 3, 6, 10, 7, 4, 9, 0, 8, 6, 4, 3, 5, 2, 1, 0, 9, 7, 10, 8, 5, 3, 2, 4, 6];
+let arrOfertas = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false];
 let arrProductos = [];
+
+// Agrega {cantidadProductosEnPromocion} ofertas al azar
+const cantidadProductosEnPromocion = 4;
+for (let i = 0; i < cantidadProductosEnPromocion; i++) {
+    let indice = (Math.floor(Math.random() * (arrProductosNombres.length + 1)));
+    arrOfertas[indice] = true;    
+    console.log(indice);
+}
 
 // Unifica los arreglos en uno
 for (let i = 0; i < arrProductosNombres.length; i++) {
     arrProductos.push([]); // Agregamos una nueva fila
-    arrProductos[i].push(arrProductosNombres[i], arrProductosPrecios[i], arrProductosStock[i]);
+    arrProductos[i].push(arrProductosNombres[i], arrProductosPrecios[i], arrProductosStock[i], arrOfertas[i]);
 }
 
-// Genera un arreglo de {cantidadProductosEnPromocion} elementos al azar
-const cantidadProductosEnPromocion = 4;
-let arrPromocion = [];
-for (let i = 0; i < cantidadProductosEnPromocion; i++) {
-    arrPromocion[i]= (Math.floor(Math.random() * (arrProductos.length + 1)));
-}
 
-// Genera un arreglo de {cantidadProductosEnPromocion} elementos al azar
-let arrTodosLosProductos = [];
-for (let i = 0; i < arrProductos.length; i++) {
-    arrTodosLosProductos[i]=i;
-}
 
-//Carga los códigos de produtos indicados por {arrProdCods} en la sección indicada por el {sectionSelector}
-function cargarProductosEnSeccion(arrProdCods, sectionSelector){
-    //Verifico que el navegador soporte <template>
-    if ("content" in document.createElement("template")) {
-        const tablaProductos = document.querySelector(sectionSelector);
-        const template = document.querySelectorAll("template")[0];
 
-        arrProdCods.forEach((codigoProducto) => {
+//Verifico que el navegador soporte <template>
+if ("content" in document.createElement("template")) {
+    const tablaProductos = document.querySelector('.seccionProductos');
+    const templateProducto = tablaProductos.querySelectorAll("template")[0];
+
+    arrProductos.forEach((producto, codigo) => {
+        //Clono una nueva fila y la agrego a la tabla
+        const nuevoProducto = templateProducto.content.cloneNode(true);
+        nuevoProducto.querySelector(".producto>div:nth-child(2)").textContent = producto[0]
+        nuevoProducto.querySelector(".producto>div:nth-child(3)").textContent = new Intl.NumberFormat('es-AR', { style: "currency", currency: "ARS" }).format(producto[1]);
+        const imgProducto = nuevoProducto.querySelector(".producto>div>div:nth-child(2)>img");
+        imgProducto.setAttribute("src", "../imagenes/producto" + codigo + ".webp");
+        imgProducto.setAttribute("height", "100%"); // VER
+        imgProducto.setAttribute("alt", producto[0]);
+        nuevoProducto.querySelector(".producto>div:nth-child(4)>input").setAttribute("id", codigo);
+        tablaProductos.appendChild(nuevoProducto);
+
+        //Si el producto está en oferta lo agrego a la sección indicada
+        if (producto[3]){            
             //Clono una nueva fila y la agrego a la tabla
-            const clone = template.content.cloneNode(true);
-            clone.querySelector(".producto>div:nth-child(2)").textContent = arrProductos[codigoProducto][0];
-            clone.querySelector(".producto>div:nth-child(3)").textContent = new Intl.NumberFormat('es-AR', { style: "currency", currency: "ARS" }).format(arrProductos[codigoProducto][1]);
-            const imgProducto = clone.querySelector(".producto>div>div:nth-child(2)>img");
-            imgProducto.setAttribute("src", "../imagenes/producto" + codigoProducto + ".webp");
+            const tablaOfertas = document.querySelector('.seccionPromociones');
+            const templateOferta = tablaOfertas.querySelectorAll("template")[0];
+            const nuevaOferta = templateOferta.content.cloneNode(true);
+            nuevaOferta.querySelector(".producto>div:nth-child(2)").textContent = producto[0]
+            nuevaOferta.querySelector(".producto>div:nth-child(3)").textContent = new Intl.NumberFormat('es-AR', { style: "currency", currency: "ARS" }).format(producto[1]);
+            const imgProducto = nuevaOferta.querySelector(".producto>div>div:nth-child(2)>img");
+            imgProducto.setAttribute("src", "../imagenes/producto" + codigo + ".webp");
             imgProducto.setAttribute("height", "100%"); // VER
-            imgProducto.setAttribute("alt", arrProductos[codigoProducto]);
-            clone.querySelector(".producto>div:nth-child(4)>input").setAttribute("id", codigoProducto);
-
-            tablaProductos.appendChild(clone);
-        });
-    }
+            imgProducto.setAttribute("alt", producto[0]);
+            tablaOfertas.appendChild(nuevaOferta);            
+        }
+    });
 }
 
-cargarProductosEnSeccion(arrPromocion, '.seccionPromociones');
-cargarProductosEnSeccion(arrTodosLosProductos, '.seccionProductos');
+//cargarProductosEnSeccion(arrPromocion, '.seccionPromociones');
+//cargarProductosEnSeccion(arrTodosLosProductos, '.seccionProductos');
 
 // // Verifico si el navegador soporta templates
 // if ("content" in document.createElement("template")) {
