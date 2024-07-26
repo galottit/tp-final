d53122f8b2039bc1f94f32d44c22d5c83dfe13bd
Tomas Galotti <galottit@gmail.com>
2024-07-26 14:31:19 -0300

    Se cambió el botón enviar en contactos a tipo submit y que valide desde el html

--- scripts/contacto.js
+++ scripts/contacto.js
@@ -8,23 +8,16 @@ let consulta = document.getElementById("cajaConsulta");
 let botonEnviar = document.getElementById("botonEnviar");
 let formularioContacto = [];
 
-botonEnviar.addEventListener("click", () => {
+
+
+document.querySelector("form").addEventListener("submit", (event) => {
     formularioContacto[0] = nombre.value;
     formularioContacto[1] = apellido.value;
     formularioContacto[2] = email.value;
     formularioContacto[3] = telefono.value;
     formularioContacto[4] = consulta.value;
-    //compruebo si alguno de los campos está vacio
-    let datosCompletos = true;
-    for(let i=0; i<formularioContacto.length; i++){
-        if(formularioContacto[i].length == 0){
-            datosCompletos = false;
-        }
-    }
-    if(datosCompletos){
-        let blob = new Blob([formularioContacto], {type: "text/plain;charset=utf-8"}); // navegador
-        //Libreria FileSaver.min.js
-        saveAs(blob, "contact.txt");
-        console.log(blob)
-    }
+    let blob = new Blob([formularioContacto], {type: "text/plain;charset=utf-8"}); // navegador
+    //Libreria FileSaver.min.js
+    saveAs(blob, "contact.txt");
+
 })
